13142a7ab4a811681f34bb90978e925a2c3caca5
mblahnik <mblahnik@uwm.edu>
2021-03-27 00:02:55 -0500

    dsa

--- python/samples/image-viewer.py
+++ python/samples/image-viewer.py
@@ -29,8 +29,8 @@ offscreen_canvas = matrix.CreateFrameCanvas()
 image.thumbnail((matrix.width-35, matrix.height-35), Image.ANTIALIAS)
 image2.thumbnail((matrix.width-35, matrix.height-35), Image.ANTIALIAS)
 
-#matrix.SetImage(image.convert('RGB'), -2, 33)
-#matrix.SetImage(image2.convert('RGB'), 37, 33)
+matrix.SetImage(image.convert('RGB'), -2, 33)
+matrix.SetImage(image2.convert('RGB'), 37, 33)
 
 graphics.DrawText(offscreen_canvas, font, 14, 20, textColor, my_text)
 matrix.SwapOnVSync(offscreen_canvas)
