2a48d16e3a9ba96a8e0444ce82d35c140a180962
mblahnik <mblahnik@uwm.edu>
2021-03-26 23:27:16 -0500

    dsa

--- python/samples/image-viewer.py
+++ python/samples/image-viewer.py
@@ -29,8 +29,8 @@ matrix = RGBMatrix(options=options)
 image.thumbnail((matrix.width-35, matrix.height-35), Image.ANTIALIAS)
 image2.thumbnail((matrix.width-35, matrix.height-35), Image.ANTIALIAS)
 
-matrix.SetImage(image.convert('RGB'), -5, 30)
-matrix.SetImage(image2.convert('RGB'), 25, 30)
+matrix.SetImage(image.convert('RGB'), -3, 30)
+matrix.SetImage(image2.convert('RGB'), 45, 30)
 
 try:
     print("Press CTRL-C to stop.")
