3f7fbf8ffaf237d55e875931e992be9741539175
mblahnik <mblahnik@uwm.edu>
2021-03-26 20:29:27 -0500

    rydry

---
+++ python/samples/__pycache__/mytest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from samplebase import SampleBase
+
+
+class MyTest(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(MyTest, self).__init__(*args, **kwargs)
+
+    def run(self):
+        offset_canvas = self.matrix.CreateFrameCanvas()
+
+        while True:
+            offset_canvas.SetPixel(15, 15, 255, 0, 0)
+
+            offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
+
+
+# Main function
+if __name__ == "__main__":
+    simple_square = MyTest()
+    if (not simple_square.process()):
+        simple_square.print_help()
