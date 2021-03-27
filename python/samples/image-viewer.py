15e55afbc3bf143124ff463cb48757c32985aa43
mblahnik <mblahnik@uwm.edu>
2021-03-27 15:05:46 -0500

    fdsfds

--- python/samples/image-viewer.py
+++ python/samples/image-viewer.py
@@ -4,7 +4,7 @@ import sys
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 from PIL import Image
 
-image = Image.open("milB.png")
+image = Image.open("https://lh3.googleusercontent.com/proxy/V6KZ6m33ku3Vdj7zsSnsi6uDIUuzfNhxgFRg4kV89P0cQ-ExSaBF5ir-kcCw8WdsxamUl1bZ7l_gNr-Nfk0t7xxhEsuIL-4Xh6IpROUyFK0XZhzWn3mvinbn4W9yYyOgjD-wzlU2a3g4Ic-TMB4p=p-h96-w96-rw")
 image2 = Image.open("whitesox.png")
 
 font = graphics.Font()
--- src/main.py
+++ src/main.py
@@ -4,8 +4,7 @@ from bs4 import BeautifulSoup
 
 def main():
     URL = 'https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNRFV4ZG5vU0FtVnVLQUFQAQ?hl=en-US&gl=US&ceid=US%3Aen'
-    URL2 = 'https://www.espn.com/nfl/team/schedule/_/name/gb'
-    URL3 = 'https://www.espn.com/mlb/boxscore?gameId=401289013'
+
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -20,20 +19,6 @@ def main():
     # print("/")
 
    # tableRows = soup.find_all('tr', class_='Table__TR')
-
-   # for i in range(1, len(tableRows)):
-    #    print(tableRows[i].findAll('td')[0].get_text())
-
-    #url = "https://therundown-therundown-v1.p.rapidapi.com/sports"
-
-    # headers = {
-    #    'x-rapidapi-key': "478c0ebbc0msh9d93f83fb216aeep1a8f36jsnfb4817fac2b8",
-    #    'x-rapidapi-host': "therundown-therundown-v1.p.rapidapi.com"
-    # }
-
-    #response = requests.request("GET", url, headers=headers)
-
-    # print(response.text)
 
 
 if __name__ == "__main__":
