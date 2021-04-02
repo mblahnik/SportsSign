34828430e3e70351dab999fd270eb6c67a2b8e46
mblahnik <mblahnik@uwm.edu>
2021-04-02 17:51:34 -0500

    fsdfs

--- src/GoogleNewsParser.py
+++ src/GoogleNewsParser.py
@@ -1,28 +1,63 @@
-from bs4 import Beauti
+from bs4 import BeautifulSoup, SoupStrainer
 import requests
+from timeout import timeout
+from PIL import Image
 
 
 class GoogleNewsParser:
     brewersNewsURL = "https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNRFV4ZG5vU0FtVnVLQUFQAQ?hl=en-US&gl=US&ceid=US%3Aen"
     googleNewsHTML = ""
     soup = None
+    card = None
+    Away_Team_Index = 0
+    Home_Team_Index = 1
 
     def __init__(self):
         super().__init__()
-        LoadPage()
 
+    @timeout(60)
     def LoadPage(self):
         page = requests.get(brewersNewsURL)
+        parse_list = SoupStrainer('div', attrs={"class": "SOsZve"})
         self.soup = BeautifulSoup(page.content, 'html.parser')
+        length = len(page.content)
+        start = round(length*.55)
+        end = round(length*.65)
+        print("GettingSoup")
+        soup = BeautifulSoup(
+            page.content[start:end], 'html.parser', parse_only=parse_list)
+        print("SoupGotten")
+        cards = soup.find_all('div', class_='SOsZve')
+        card = card[len(cards)-1]
 
     def GetHomeTeamLogo(self):
-        return None
+        TeamLogos = card.find_all('img')
+        Home_Team_Logo_URL = TeamLogos[Home_Team_Index]['src']
+        return Image.open(requests.get(Home_Team_Logo_URL, stream=True).raw)
 
     def GetHomeTeamScore(self):
-        return None
+        Scores = card.find_all('div', class_='nE4ijc')
+        return Scores[Home_Team_Index].string
 
     def GetAwayTeamLogo(self):
-        return None
+        TeamLogos = card.find_all('img')
+        Away_Team_Logo_URL = TeamLogos[Home_Team_Index]['src']
+        return Image.open(requests.get(Away_Team_Logo_URL, stream=True).raw)
 
     def GetAwayTeamScore(self):
-        return None
+        Scores = card.find_all('div', class_='nE4ijc')
+        return Scores[Home_Team_Index].string
+
+    def GetInning(self):
+        InningTag = card.find_all('div', class_='MHBqCc uULV7d')
+        InningText = ""
+        if len(InningTag) > 0:
+            InningText = InningTag[0].string
+        return InningText
+
+    def GetAdditionalText(self):
+        AdditionalTextTags = card.find_all('div', class_='njYF6e')
+        AdditionalText = []
+        for tag in AdditionalTextTags:
+            AdditionalText.append(tag.string)
+        return AdditionalText
--- src/main.py
+++ src/main.py
@@ -6,11 +6,13 @@ import time
 from Scene import Scene
 from MLBSceneRenderer import MLBSceneRenderer
 from PIL import Image
+from GoogleNewsParser import GoogleNewsParser
 
 
 def main():
 
     sceneRenderer = MLBSceneRenderer()
+    parser = GoogleNewsParser()
 
     URL = 'https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNRFV4ZG5vU0FtVnVLQUFQAQ?hl=en-US&gl=US&ceid=US%3Aen'
     URL2 = 'https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNRGRzT0dZU0FtVnVLQUFQAQ?hl=en-US&gl=US&ceid=US%3Aen'
@@ -22,58 +24,61 @@ def main():
 
     while True:
         try:
-            page = requests.get(URL2)
+            parser.LoadPage()
+            #page = requests.get(URL2)
 
-            parse_list = SoupStrainer('div', attrs={"class": "SOsZve"})
+            #parse_list = SoupStrainer('div', attrs={"class": "SOsZve"})
 
-            length = len(page.content)
-            start = round(length*.55)
-            end = round(length*.65)
-            print("GettingSoup")
-            soup = BeautifulSoup(
-                page.content[start:end], 'html.parser', parse_only=parse_list)
-            print("SoupGotten")
-            r = soup.find_all('div', class_='SOsZve')
-            r = r[len(r)-1]
+            #length = len(page.content)
+            #start = round(length*.55)
+            #end = round(length*.65)
+            # print("GettingSoup")
+            # soup = BeautifulSoup(
+            # page.content[start:end], 'html.parser', parse_only=parse_list)
+            # print("SoupGotten")
+            #r = soup.find_all('div', class_='SOsZve')
+            #r = r[len(r)-1]
 
-            TeamLogos = r.find_all('img')
-            Home_Team_Logo_URL = TeamLogos[Home_Team_Index]['src']
-            Away_Team_Logo_URL = TeamLogos[Away_Team_Index]['src']
+            #TeamLogos = r.find_all('img')
+            #Home_Team_Logo_URL = TeamLogos[Home_Team_Index]['src']
+            #Away_Team_Logo_URL = TeamLogos[Away_Team_Index]['src']
 
-            Scores = r.find_all('div', class_='nE4ijc')
+            #Scores = r.find_all('div', class_='nE4ijc')
 
-            Home_Team_Score = Scores[Home_Team_Index].string
-            Away_Team_Score = Scores[Away_Team_Index].string
+            #Home_Team_Score = Scores[Home_Team_Index].string
+            #Away_Team_Score = Scores[Away_Team_Index].string
 
-            AdditionalTextTags = r.find_all('div', class_='njYF6e')
-            AdditionalText = []
+            #AdditionalTextTags = r.find_all('div', class_='njYF6e')
+            #AdditionalText = []
 
-            for tag in AdditionalTextTags:
-                AdditionalText.append(tag.string)
+            # for tag in AdditionalTextTags:
+            # AdditionalText.append(tag.string)
 
-            InningTag = r.find_all('div', class_='MHBqCc uULV7d')
-            InningText = ""
+            #InningTag = r.find_all('div', class_='MHBqCc uULV7d')
+            #InningText = ""
 
-            if len(InningTag) > 0:
-                InningText = InningTag[0].string
+            # if len(InningTag) > 0:
+            #InningText = InningTag[0].string
 
             # Scene starts here
-            Home_Team_Logo_Image = Image.open(
-                requests.get(Home_Team_Logo_URL, stream=True).raw)
-            Away_Team_Logo_Image = Image.open(
-                requests.get(Away_Team_Logo_URL, stream=True).raw)
+            # Home_Team_Logo_Image = Image.open(
+            # requests.get(Home_Team_Logo_URL, stream=True).raw)
+            # Away_Team_Logo_Image = Image.open(
+            # requests.get(Away_Team_Logo_URL, stream=True).raw)
 
             scene = Scene()
-            scene.Home_Team_Logo_Image = Home_Team_Logo_Image
-            scene.Away_Team_Logo_Image = Away_Team_Logo_Image
-            scene.Home_Team_Score = Home_Team_Score
-            scene.Away_Team_Score = Away_Team_Score
-            scene.InningText = InningText
-            scene.AdditionalText = AdditionalText
+            scene.Home_Team_Logo_Image = parser.GetHomeTeamLogo
+            scene.Away_Team_Logo_Image = parser.GetAwayTeamLogo
+            scene.Home_Team_Score = parser.GetHomeTeamScore
+            scene.Away_Team_Score = parser.GetAwayTeamScore
+            scene.InningText = parser.GetInning
+            scene.AdditionalText = parser.GetAdditionalText
 
             sceneRenderer.RenderScene(scene)
         except KeyboardInterrupt:
             quit()
+        except TimeoutError:
+            print("Request taking too long, starting over")
         except:
             print("Error, trying again")
 
