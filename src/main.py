4deed5e2d9a9a913d519184bd942688066c1cb8b
mblahnik <mblahnik@uwm.edu>
2021-04-02 16:33:50 -0500

    fdsfsd

--- src/main.py
+++ src/main.py
@@ -19,59 +19,57 @@ def main():
     Home_Team_Index = 1
 
     while True:
-        try:
-            page = requests.get(URL2)
 
-            parse_list = SoupStrainer('div', attrs={"class": "SOsZve"})
+        page = requests.get(URL2)
 
-            length = len(page.content)
-            start = round(length*.55)
-            end = round(length*.65)
-            print("GettingSoup")
-            soup = BeautifulSoup(
-                page.content[start:end], 'html.parser', parse_only=parse_list)
-            print("SoupGotten")
-            r = soup.find_all('div', class_='SOsZve')
-            r = r[len(r)-1]
+        parse_list = SoupStrainer('div', attrs={"class": "SOsZve"})
 
-            TeamLogos = r.find_all('img')
-            Home_Team_Logo_URL = TeamLogos[Home_Team_Index]['src']
-            Away_Team_Logo_URL = TeamLogos[Away_Team_Index]['src']
+        length = len(page.content)
+        start = round(length*.55)
+        end = round(length*.65)
+        print("GettingSoup")
+        soup = BeautifulSoup(
+            page.content[start:end], 'html.parser', parse_only=parse_list)
+        print("SoupGotten")
+        r = soup.find_all('div', class_='SOsZve')
+        r = r[len(r)-1]
 
-            Scores = r.find_all('div', class_='nE4ijc')
+        TeamLogos = r.find_all('img')
+        Home_Team_Logo_URL = TeamLogos[Home_Team_Index]['src']
+        Away_Team_Logo_URL = TeamLogos[Away_Team_Index]['src']
 
-            Home_Team_Score = Scores[Home_Team_Index].string
-            Away_Team_Score = Scores[Away_Team_Index].string
+        Scores = r.find_all('div', class_='nE4ijc')
 
-            AdditionalTextTags = r.find_all('div', class_='njYF6e')
-            AdditionalText = []
+        Home_Team_Score = Scores[Home_Team_Index].string
+        Away_Team_Score = Scores[Away_Team_Index].string
 
-            for tag in AdditionalTextTags:
-                AdditionalText.append(tag.string)
+        AdditionalTextTags = r.find_all('div', class_='njYF6e')
+        AdditionalText = []
 
-            InningTag = r.find_all('div', class_='MHBqCc uULV7d')
-            InningText = ""
+        for tag in AdditionalTextTags:
+            AdditionalText.append(tag.string)
 
-            if len(InningTag) > 0:
-                InningText = InningTag[0].string
+        InningTag = r.find_all('div', class_='MHBqCc uULV7d')
+        InningText = ""
 
-            # Scene starts here
-            Home_Team_Logo_Image = Image.open(
-                requests.get(Home_Team_Logo_URL, stream=True).raw)
-            Away_Team_Logo_Image = Image.open(
-                requests.get(Away_Team_Logo_URL, stream=True).raw)
+        if len(InningTag) > 0:
+            InningText = InningTag[0].string
 
-            scene = Scene()
-            scene.Home_Team_Logo_Image = Home_Team_Logo_Image
-            scene.Away_Team_Logo_Image = Away_Team_Logo_Image
-            scene.Home_Team_Score = Home_Team_Score
-            scene.Away_Team_Score = Away_Team_Score
-            scene.InningText = InningText
-            scene.AdditionalText = AdditionalText
+        # Scene starts here
+        Home_Team_Logo_Image = Image.open(
+            requests.get(Home_Team_Logo_URL, stream=True).raw)
+        Away_Team_Logo_Image = Image.open(
+            requests.get(Away_Team_Logo_URL, stream=True).raw)
 
-            sceneRenderer.RenderScene(scene)
-        except:
-            print("Error...trying again")
+        scene = Scene()
+        scene.Home_Team_Logo_Image = Home_Team_Logo_Image
+        scene.Away_Team_Logo_Image = Away_Team_Logo_Image
+        scene.Home_Team_Score = Home_Team_Score
+        scene.Away_Team_Score = Away_Team_Score
+        scene.InningText = InningText
+        scene.AdditionalText = AdditionalText
+
+        sceneRenderer.RenderScene(scene)
 
 
 if __name__ == "__main__":
