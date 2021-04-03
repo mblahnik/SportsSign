f714448e0aa2a1841f2adceeb3a9f57cdd2e2234
mblahnik <mblahnik@uwm.edu>
2021-04-03 02:02:29 -0500

    fdsfs

--- src/MLBSceneRenderer.py
+++ src/MLBSceneRenderer.py
@@ -17,6 +17,7 @@ class MLBSceneRenderer:
         options.parallel = 1
         options.pixel_mapper_config = "U-mapper"
         options.hardware_mapping = 'adafruit-hat-pwm'
+        options.pwm_lsb_nanoseconds = 130
         self.matrix = RGBMatrix(options=options)
         self.buffer = self.matrix.CreateFrameCanvas()
 
