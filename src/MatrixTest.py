f38fc9ec49115cbbdda2b8ab75eb55b6bf8ffe59
mblahnik <mblahnik@uwm.edu>
2021-04-03 12:52:01 -0500

    dsad

---
+++ src/MatrixTest.py
@@ -0,0 +1,27 @@
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+from PIL import Image
+import time
+import os
+
+
+options = RGBMatrixOptions()
+options.rows = 32
+options.cols = 64
+options.chain_length = 2
+options.pixel_mapper_config = "U-mapper"
+options.hardware_mapping = 'adafruit-hat-pwm'
+options.pwm_lsb_nanoseconds = 130
+options.gpio_slowdown = 0
+matrix = RGBMatrix(options=options)
+buffer = matrix.CreateFrameCanvas()
+
+
+image = Image.Open("./TeamLogos/MLB/Brewers.png")
+
+image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+
+matrix.SetImage(image.convert('RGB'))
+
+
+while True:
+    time.sleep(100)
