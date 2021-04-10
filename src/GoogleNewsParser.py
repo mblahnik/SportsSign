091f3adce658b1df5f7122937d7b40c2722ac8e9
mblahnik <mblahnik@uwm.edu>
2021-04-10 16:49:26 -0500

    fds

--- src/GoogleNewsParser.py
+++ src/GoogleNewsParser.py
@@ -28,7 +28,7 @@ class GoogleNewsParser:
             page.content[start:end], 'html.parser', parse_only=parse_list)
         cards = self.soup.find_all('div', class_='SOsZve')
         self.card = cards[len(cards)-1]
-        self.UpcomingGameCard = soup.find('div', class_='LI3zEe uP1HId')
+        self.UpcomingGameCard = self.soup.find('div', class_='LI3zEe uP1HId')
 
     def GetHomeTeamLogo(self):
         hometeamName = self.card.findAll('div', class_='MlH7je')[
