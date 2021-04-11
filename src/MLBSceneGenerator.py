500753091db498d64eaced75f91f3aab1ff1eba8
mblahnik <mblahnik@uwm.edu>
2021-04-10 19:32:07 -0500

    d

--- src/MLBSceneGenerator.py
+++ src/MLBSceneGenerator.py
@@ -18,10 +18,12 @@ class MLBSceneGenerator:
         upComingDateString = "Sat, Apr 10 8:10 PM"
 
         if self.shouldDisplayUpComingGame(upComingDateString):
+            print("Showing upcoming game")
             scene.Home_Team_Logo_Image = self.dataSource.GetUpcomingGameHomeTeamLogo()
             scene.Away_Team_Logo_Image = self.dataSource.GetUpcomingGameAwayTeamLogo()
             scene.MainText = upComingDateString
         else:
+            print("Showing current game")
             scene.Home_Team_Logo_Image = self.dataSource.GetHomeTeamLogo()
             scene.Away_Team_Logo_Image = self.dataSource.GetAwayTeamScore()
             scene.Home_Team_Score = self.dataSource.GetHomeTeamScore()
