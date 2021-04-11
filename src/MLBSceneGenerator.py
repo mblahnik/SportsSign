dd88e018f22a5585a6466752b47d03554be24c6e
mblahnik <mblahnik@uwm.edu>
2021-04-10 19:47:49 -0500

    dasda

--- src/MLBSceneGenerator.py
+++ src/MLBSceneGenerator.py
@@ -41,5 +41,5 @@ class MLBSceneGenerator:
         nextGameDate = datetime.datetime(
             year, dateDict[dateSplit[1]], int(dateSplit[2]))
 
-        return currentDate.date().day == nextGameDate.date().day and (
-            currentDate.date().month == nextGameDate.date().month) and currentDate.date().year == nextGameDate.date().year
+        return (currentDate.date().day == nextGameDate.date().day and (
+            currentDate.date().month == nextGameDate.date().month) and currentDate.date().year == nextGameDate.date().year)
