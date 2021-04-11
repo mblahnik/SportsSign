f44afa67fb27e0b0776fd5ee58ce3b592d75a0b6
mblahnik <mblahnik@uwm.edu>
2021-04-11 18:52:25 -0500

    aa

--- src/MLBLogoBadPixels.py
+++ src/MLBLogoBadPixels.py
@@ -145,7 +145,9 @@ OriolesLogoBadPixels = [Pixel(0, 5), Pixel(0, 6), Pixel(1, 4), Pixel(1, 3), Pixe
                         Pixel(20, 28), Pixel(21, 27), Pixel(21, 26), Pixel(22, 26), Pixel(
                             22, 27), Pixel(23, 26), Pixel(23, 25), Pixel(24, 24), Pixel(26, 19),
                         Pixel(25, 23), Pixel(25, 22), Pixel(25, 21), Pixel(25, 19), Pixel(26, 20), Pixel(26, 18), Pixel(27, 18), Pixel(27, 17), Pixel(28, 17), Pixel(29, 17), Pixel(30, 15)]
-PhilliesLogoBadPixels = []
+
+PhilliesLogoBadPixels = [Pixel(0, 3), Pixel(0, 4), Pixel(1, 2), Pixel(2, 1), Pixel(3, 0), Pixel(4, 0), Pixel(0, 14), Pixel(1, 15), Pixel(2, 16), Pixel(3, 16),
+                         Pixel(4, 16), Pixel(5, 16), Pixel(6, 15), Pixel(5, 17), Pixel(5, 18), Pixel(5, 19), Pixel(5, 20), Pixel(5, 21), Pixel(5, 22), Pixel(5, 23)]
 RangersLogoBadPixels = []
 RaysLogoBadPixels = []
 RedsLogoBadPixels = []
