5aab3e2d058ae1d2932ebb1e99cb40708ac584e0
mblahnik <mblahnik@uwm.edu>
2021-04-11 17:02:49 -0500

    dsad

--- src/MLBLogoBadPixels.py
+++ src/MLBLogoBadPixels.py
@@ -111,7 +111,6 @@ MarlinsLogoBadPixels = [Pixel(7, 0), Pixel(7, 1), Pixel(7, 2), Pixel(7, 3), Pixe
                         Pixel(28, 15), Pixel(27, 17), Pixel(27, 18), Pixel(26, 20), Pixel(26, 21), Pixel(
                             26, 22), Pixel(25, 23), Pixel(25, 24), Pixel(25, 25), Pixel(25, 26), Pixel(25, 27),
                         Pixel(24, 26), Pixel(24, 27)]
-MetsLogoBadPixels = []
 NationalsLogoBadPixels = []
 OriolesLogoBadPixels = []
 PhilliesLogoBadPixels = []
@@ -123,6 +122,7 @@ TigersLogoBadPixels = []
 TwinsLogoBadPixels = []
 WhiteSoxLogoBadPixels = []
 YankeesLogoBasPixels = []
+MetsLogoBadPixels = []
 
 teamsDict = {"Brewers.png": BrewersLogoBadPixels,
              "Angels.png": AngelsLogoBadPixels,
