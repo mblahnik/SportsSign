fc44a767e9e38bd69cf278346a8decaf5a4f0145
mblahnik <mblahnik@uwm.edu>
2021-04-16 20:17:49 -0500

    jj

--- src/MLBLogoBadPixels.py
+++ src/MLBLogoBadPixels.py
@@ -245,7 +245,7 @@ MetsLogoBadPixels = [Pixel(0, 24), Pixel(1, 24), Pixel(
         0, 17), Pixel(1, 17), Pixel(6, 4), Pixel(7, 5), Pixel(8, 6), Pixel(9, 7),
     Pixel(13, 17), Pixel(12, 16)]
 TigersLogoBadPixels = [Pixel(3, 0), Pixel(4, 0), Pixel(5, 1), Pixel(6, 1), Pixel(
-    7, 1), Pixel(8, 1), Pixel(9, 1), Pixel(11, 0), Pixel(12, 0), Pixel(13, 0)]
+    7, 1), Pixel(8, 1), Pixel(9, 1), Pixel(11, 0), Pixel(12, 0), Pixel(13, 0), Pixel(10, 1), Pixel(14, 0), Pixel(17, 1), Pixel(18, 2), Pixel(19, 3)]
 TwinsLogoBadPixels = []
 WhiteSoxLogoBadPixels = []
 YankeesLogoBasPixels = []
