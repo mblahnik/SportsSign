a4d703aa6f557b0713f209b00e9b8fc003b70649
Your Name <mblahnik@uwm.edu>
2021-09-17 12:51:05 -0500

    Fix typo

--- src/NFLSceneGenerator.py
+++ src/NFLSceneGenerator.py
@@ -6,7 +6,7 @@ dateDict = {'Jan': 1, 'Fed': 2, 'Mar': 3,
             'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12}
 
 
-class MLBSceneGenerator:
+class NFLSceneGenerator:
     def __init__(self):
         super().__init__()
         self.dataSource = NFLGoogleNewsParser()
